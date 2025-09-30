b7600ee92b8c25d789e7775102663fda9e93eef0
Dioss1718 <diyasuryawanshi1804@gmail.com>
2025-09-30 07:09:15 +0530

    fixed path issue

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,17 +5,15 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  base: "/",
   build: {
-    outDir: "build",
-    chunkSizeWarningLimit: 2000,
+    outDir: "build", 
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
     port: "4028",
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
-  }
+    allowedHosts: [".amazonaws.com", ".builtwithrocket.new"],
+  },
 });
